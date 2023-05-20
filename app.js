e6b3854a6bcf94f27138e8c9043aa3cbafef3d29
ImeneRdn <imene.ramdan@live.fr>
2023-05-20 16:43:39 +0200

    delete

--- app.js
+++ app.js
@@ -18,13 +18,19 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
 app.get("/api/users", userHandlers.getUser);
 app.get("/api/users/:id", userHandlers.getUserById);
 
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", userHandlers.postUser);
+
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
+
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
